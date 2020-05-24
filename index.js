d0bc0fc88cbf9e986ef89c4ba9a67c0864c70804
animesh0911 <anni.landge@gmail.com>
2020-05-24 22:38:33 +0530

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://anni0911:fuehrer007@cluster0-dwsf6.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key')
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
     {useNewUrlParser : true, useUnifiedTopology : true}).then(() => {
         console.log("DB connected");
     }).catch((err) => {
         console.error(err);
     });
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if(err) return res.json({success : false, err});
+        return res.status(200).json({success : true, userData : doc});
+    });
 });
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -13,7 +13,7 @@ const userSchema = mongoose.Schema({
     },
     password : {
         type : String,
-        minLength = 5
+        minLength : 5
     },
     lastName : {
         type : String,
