076c8077978a03ad4af9ab9d637037319f86bccb
animesh0911 <anni.landge@gmail.com>
2020-05-25 11:34:47 +0530

    added login

--- index.js
+++ index.js
@@ -21,9 +21,33 @@ app.use(cookieParser());
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
+
     user.save((err, doc) => {
         if(err) return res.json({success : false, err});
         return res.status(200).json({success : true, userData : doc});
+    });
+});
+
+app.post('/api/users/login', (req, res) => {
+    User.findOne({ email : req.body.email}, (err, user) => {
+        if(!user){
+            return res.json({
+                loginSuccess : false,
+                message : "Auth failed, email not found"
+            });
+        }
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({
+                    loginSuccess : false,
+                    message : "wrong password"
+                });
+            }
+        });
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token).status(200).json({loginSuccess : true});
+        });
     });
 });
 
