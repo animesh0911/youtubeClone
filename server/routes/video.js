738f856cbade9888a7eb452c4e614f64d0c548d8
animesh0911 <anni.landge@gmail.com>
2020-06-17 16:07:00 +0530

    .

--- server/routes/video.js
+++ server/routes/video.js
@@ -78,7 +78,15 @@ router.post('/uploadVideo', (req, res) => {
             return res.status(200).json({success : true});
         }
     });
+});
 
+router.get("/getVideos", (req, res) => {
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
 });
 
 
